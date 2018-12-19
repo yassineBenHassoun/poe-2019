070bb789665aec99dff2fa1fa6abfafab6d750b0
alanmahjoub <31803806+alanmahjoub@users.noreply.github.com>
2018-12-19 13:55:56 +0100

    Created webpackconfig, json and index.js (#54)
    * edited webpack config, json and index.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,13 @@ module.exports = {
   mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({template: './src/index.html'})
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
