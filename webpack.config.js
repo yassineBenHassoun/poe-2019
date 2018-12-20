a58ffe1da024960cab6fd2865b979c674b941fc7
BenHassoun <y.benhassoun@gmail.com>
2018-12-20 15:19:47 +0100

    config

--- webpack.config.js
+++ webpack.config.js
@@ -23,15 +23,11 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              // you can specify a publicPath here
-              // by default it use publicPath in webpackOptions.output
-              publicPath: '../'
-            }
-          },
-          'css-loader'
+
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'postcss-loader'
+
         ]
       },
       {
