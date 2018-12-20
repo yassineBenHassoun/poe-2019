77c9d57f8c1846a7d2eb369162738614417d6521
Yvain Liechti <yliechti@clever-age.com>
2018-12-20 10:43:59 +0100

    eslint

--- src/index.js
+++ src/index.js
@@ -1,48 +1,45 @@
-require('./style.scss');
-console.log('Hello World');
+require('./style.scss')
+console.log('Hello World')
 
+const responsiveNav = require('responsive-nav')
+require('responsive-nav/client/dist/styles/responsive-nav.css')
 
-const responsiveNav = require('responsive-nav');
-require('responsive-nav/client/dist/styles/responsive-nav.css');
+responsiveNav('nav')
 
-const nav = responsiveNav('nav');
-
-const mq = window.matchMedia('(min-width: 35em)');
+const mq = window.matchMedia('(min-width: 35em)')
 
 const loadPartial = () => {
-  const link = document.querySelector('[data-ajax-replace]');
+  const link = document.querySelector('[data-ajax-replace]')
   if (!link) {
-    return;
+    return
   }
-  const url = link.getAttribute('data-ajax-replace');
+  const url = link.getAttribute('data-ajax-replace')
 
   const options = {
     headers: new Headers()
-  };
+  }
 
-  options.headers.append('Content-Type', 'application/json');
-  options.headers.append('X-Requested-With', 'XMLHttpRequest');
+  options.headers.append('Content-Type', 'application/json')
+  options.headers.append('X-Requested-With', 'XMLHttpRequest')
 
   return fetch(url, options)
     .then((response) => {
-        if (response.ok) {
-            return response;
-        }
+      if (response.ok) {
+        return response
+      }
 
-        const error = new Error(response.statusText);
-        error.response = response;
-        throw error;
+      const error = new Error(response.statusText)
+      error.response = response
+      throw error
     })
     .then((response) => {
       response.text().then((text) => {
-        link.insertAdjacentHTML('afterend', text);
-        link.parentNode.removeChild(link);
-      });
-    });
+        link.insertAdjacentHTML('afterend', text)
+        link.parentNode.removeChild(link)
+      })
+    })
 }
-
 
 if (mq.matches) {
-  loadPartial();
+  loadPartial()
 }
-
