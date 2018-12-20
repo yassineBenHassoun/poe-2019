795503166c4842b31e475bc457ac9027b1b4f6c9
Yvain Liechti <yliechti@clever-age.com>
2018-12-20 15:47:32 +0100

    postcss

--- webpack.config.js
+++ webpack.config.js
@@ -23,29 +23,17 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              // you can specify a publicPath here
-              // by default it use publicPath in webpackOptions.output
-              publicPath: '../'
-            }
-          },
-          'css-loader'
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader'
         ]
       },
       {
         test: /\.scss$/,
         use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              // you can specify a publicPath here
-              // by default it use publicPath in webpackOptions.output
-              publicPath: '../'
-            }
-          },
+          MiniCssExtractPlugin.loader,
           'css-loader',
+          'postcss-loader',
           'sass-loader'
         ]
       }
